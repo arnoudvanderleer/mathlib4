62102ddcfef37f605980cad23488e4f70c159589
Arnoud van der Leer <tempestasludi@gmail.com>
2026-02-14 15:09:36 +0100

    Fix the indentation and leading colons in StdSimplex

--- Mathlib/AlgebraicTopology/SimplicialSet/StdSimplex.lean
+++ Mathlib/AlgebraicTopology/SimplicialSet/StdSimplex.lean
@@ -193,19 +193,18 @@ lemma yonedaEquiv_comp {X Y : SSet.{u}} {n : SimplexCategory}
     yonedaEquiv (f ≫ g) = g.app _ (yonedaEquiv f) := rfl
 
 /-- `yonedaEquiv` is natural. -/
-lemma yonedaEquiv_naturality
-  {X : SSet} {m n : SimplexCategory} (f : m ⟶ n) (g : stdSimplex.obj n ⟶ X)
-  : X.map f.op (yonedaEquiv g) = yonedaEquiv (stdSimplex.map f ≫ g)
-  := uliftYonedaEquiv_naturality _ _
+lemma yonedaEquiv_naturality {X : SSet} {m n : SimplexCategory}
+    (f : m ⟶ n) (g : stdSimplex.obj n ⟶ X) :
+    X.map f.op (yonedaEquiv g) = yonedaEquiv (stdSimplex.map f ≫ g) :=
+  uliftYonedaEquiv_naturality _ _
 
 /-- `yonedaEquiv.symm` is natural -/
-lemma yonedaEquiv_symm_naturality
-  {X : SSet} {m n : SimplexCategory} (f : m ⟶ n) (g : X.obj (Opposite.op n))
-  : stdSimplex.map f ≫ yonedaEquiv.symm g = yonedaEquiv.symm (X.map f.op g)
-  := by
-    rw [← yonedaEquiv.apply_eq_iff_eq_symm_apply]
-    rw [← yonedaEquiv_naturality]
-    rw [yonedaEquiv.apply_symm_apply]
+lemma yonedaEquiv_symm_naturality {X : SSet} {m n : SimplexCategory}
+    (f : m ⟶ n) (g : X.obj (Opposite.op n)) :
+    stdSimplex.map f ≫ yonedaEquiv.symm g = yonedaEquiv.symm (X.map f.op g) := by
+  rw [← yonedaEquiv.apply_eq_iff_eq_symm_apply]
+  rw [← yonedaEquiv_naturality]
+  rw [yonedaEquiv.apply_symm_apply]
 
 namespace Subcomplex
 
